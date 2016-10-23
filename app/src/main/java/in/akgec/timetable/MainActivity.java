3cb2c0ba0cc34ed355341fdfa519cec9b43b496d
rajat19 <rajatsri94@gmail.com>
2016-10-23 15:11:52 +0530

    time table app
    app for akgec timetable

---
+++ app/src/main/java/in/akgec/timetable/MainActivity.java
@@ -0,0 +1,46 @@
+package in.akgec.timetable;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView textView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.add(R.id.main_container, new HomeFragment());
+        fragmentTransaction.commit();
+        textView = new TextView(getApplicationContext());
+        textView.setText("Samarpan");
+        textView.setTextSize(30);
+        textView.setTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(textView);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+    }
+}
